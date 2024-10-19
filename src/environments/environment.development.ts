ad92138f34a94d4ee584caaf60e3f52f0de821ff
jhonathan55 <jfgutierrezsaico@gmail.com>
2024-10-18 23:31:47 -0300

    firebase host

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000/',
+    firebase: {
+        apiKey: 'AIzaSyDXw242b0dvefkR2uS3gtmnKAfvRZJV1jw',
+        authDomain: 'gasficenter-8be02.firebaseapp.com',
+        projectId: 'gasficenter-8be02',
+        storageBucket: 'gasficenter-8be02.appspot.com',
+        messagingSenderId: '818366212024',
+        appId: '1:818366212024:web:6adc941c67a2b7f7f6514d',
+        measurementId: 'G-PQCKKMNK45',
+      },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://gasficenter.cl/',
+  firebase: {
+    apiKey: 'AIzaSyDXw242b0dvefkR2uS3gtmnKAfvRZJV1jw',
+    authDomain: 'gasficenter-8be02.firebaseapp.com',
+    projectId: 'gasficenter-8be02',
+    storageBucket: 'gasficenter-8be02.appspot.com',
+    messagingSenderId: '818366212024',
+    appId: '1:818366212024:web:6adc941c67a2b7f7f6514d',
+    measurementId: 'G-PQCKKMNK45',
+  },
+
+  version: '20221129112743',
+};
